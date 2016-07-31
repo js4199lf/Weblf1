5cc27a6a286179540eedd65e5bbf6dc7d7b408af
Administrator <Administrator@PC-20160110BMVM>
2016-07-31 10:45:25 +0800

    new pro

---
+++ Weblf1/Weblf1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Weblf1.MvcApplication" Language="C#" %>
---
+++ Weblf1/Weblf1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Weblf1.Startup))]
+namespace Weblf1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
